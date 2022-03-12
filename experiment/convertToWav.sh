24a52079a308d458f5f8f54bb1ad7cfb5f9ec07a
Michael Wagner <mchlwgnr@gmail.com>
2022-03-12 15:36:50 -0500

    fixed typo in index.html

--- experiment/convertToWav.sh
+++ experiment/convertToWav.sh
@@ -1,3 +1,2 @@
-for i in data/recordedFiles/*.webm; do ffmpeg -i "$i" -ac 1 -f wav "${i%.*}.wav"; done
-mv data/recordedFiles/*.wav data/recordedFilesWav
-cp data/recordedFiles/*.lab data/recordedFilesWav
+for i in dataSound/*.webm; do ffmpeg -i "$i" -ac 1 -f wav "${i%.*}.wav"; done
+
