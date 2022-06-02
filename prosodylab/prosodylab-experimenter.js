2d62216a3839339dd3ebbce170d87787e68c4add
Michael Wagner <mchlwgnr@gmail.com>
2022-06-02 16:05:43 -0400

    better way to transpose

--- prosodylab/prosodylab-experimenter.js
+++ prosodylab/prosodylab-experimenter.js
@@ -183,43 +183,14 @@ prosodylab = {
     return txt.data;
   },
 
-  transpose: function(a) {
-
-  // Calculate the width and height of the Array
-  var w = a.length || 0;
-  var h = a[0] instanceof Array ? a[0].length : 0;
-
-  // In case it is a zero matrix, no transpose routine needed.
-  if(h === 0 || w === 0) { return []; }
-
-  /**
-   * @var {Number} i Counter
-   * @var {Number} j Counter
-   * @var {Array} t Transposed data is stored in this array.
-   */
-  var i, j, t = [];
-
-  // Loop through every item in the outer array (height)
-  for(i=0; i<h; i++) {
-
-    // Insert a new row (array)
-    t[i] = [];
-
-    // Loop through every item per item in outer array (width)
-    for(j=0; j<w; j++) {
-
-      // Save transposed data.
-      t[i][j] = a[j][i];
-    }
-  }
-
-  return t;
-},
 
   getMessages: function(language){
   	// load messages spreadsheet and return messages from desired language
     var allMessages = this.loadCSV(`prosodylab/messages.tsv`,false);
-    var allMessages = this.transpose(allMessages);
+    
+    // transpose array
+    allMessages = allMessages[0].map((_, colIndex) => allMessages.map(row => row[colIndex]));
+
  
     var languages  = allMessages.map(x => x[0]);
     var thisLanguage = languages.indexOf(language);
